77b05ae4e278b72a013a5c76544669910b9e1d79
Leticia Sontay <lsontayc1@miumg.edu.gt>
2024-04-14 13:50:48 -0600

    agregado modelo.SeccionDAO

--- src/main/java/modelo/SeccionDAO.java
+++ src/main/java/modelo/SeccionDAO.java
@@ -25,5 +25,37 @@ public class SeccionDAO {
     private static final String SQL_DELETE = "DELETE FROM secciones WHERE codigo_seccion=?";
     private static final String SQL_QUERY = "SELECT codigo_seccion, nombre_seccion, estatus_seccion FROM secciones WHERE codigo_seccion = ?";
 
-    
+        public List<Seccion> select() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Seccion seccion = null;
+        List<Seccion> secciones = new ArrayList<Seccion>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                String codigo = rs.getString("codigo_seccion");
+                String nombre = rs.getString("nombre_seccion");
+                String estatus = rs.getString("estatus_seccion");
+                
+                seccion = new Seccion();
+                seccion.setCodigo_seccion(codigo);
+                seccion.setNombre_seccion(nombre);
+                seccion.setEstatus_seccion(estatus);
+                
+                secciones.add(seccion);            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+
+        return secciones;
+    }
 }
